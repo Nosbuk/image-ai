a518fd42480fb2c7b37db52cf4b2830b00b89125
JM <jakub.matwis@gmail.com>
2023-01-06 11:37:59 -0800

    feat: OpenAI Controller setup with custom prompts and sizes

--- index.ts
+++ index.ts
@@ -4,6 +4,9 @@ const port = process.env.PORT || 5000;
 
 const app: Express = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
 app.use("/openai", require("./routes/openaiRoute"))
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
